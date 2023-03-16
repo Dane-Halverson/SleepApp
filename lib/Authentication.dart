914b5cd3ec70c713920cf9a29baf70727c504a18
Dane Halverson <halve564@d.umn.edu>
2023-03-16 01:08:26 -0500

    added create user and login functions to Authentication class

--- lib/Authentication.dart
+++ lib/Authentication.dart
@@ -3,4 +3,25 @@ import 'package:firebase_auth/firebase_auth.dart';
 
 class Authentication {
 
+  Future<UserCredential> createUser(String email, String password) async {
+    final credential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+          email: email, password: password);
+    return credential;
+  }
+
+  void signIn(String email, String password) async {
+    try {
+      final credential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+          email: email,
+          password: password
+      );
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        print('No user found for that email.');
+      } else if (e.code == 'wrong-password') {
+        print('Wrong password provided for that user.');
+      }
+    }
+  }
+
 }
