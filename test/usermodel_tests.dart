b1fbb84581275ad452c5660ebd086ae356955973
Ryanaldo34 <ryanaldo34@gmail.com>
2023-03-18 12:27:21 -0500

    Basic user model created
    + Model pool for storing database document references for writing
    + UserModel class for user models and a function that creates a document reference that can read and write with this custom class to be stored in model pool
    + DocumentModel class that all models inherit from and provides basic interface that is required for being able to read and write data to database

---
+++ test/usermodel_tests.dart
@@ -0,0 +1,36 @@
+import 'package:test/test.dart';
+import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import '../lib/models/models.dart';
+
+void main() async {
+  final database = FakeFirebaseFirestore();
+  final userCollection = database.collection('users');
+  final docRef = getUserDocRef(userCollection, 'ryanaldo34');
+  await docRef.set(new UserModel(
+    'Ryan',
+    'Monahan',
+    'ryanaldo34@gmail.com',
+    21,
+  )); // create the doc data, calls the save() method on the model class
+
+  test('Tests creating and retrieving a user document in the db', () async {
+    var doc = await docRef.get();
+    var user = doc.data(); // calls the fromDB method and gets the data from the db
+
+    expect(user?.firstname, 'Ryan');
+    expect(user?.lastname, 'Monahan');
+  });
+
+  test('Tests setting and retrieving models from the model pool', () async {
+    final pool = new ModelPool();
+    var model = await docRef.get();
+
+    pool.insertModel('ryanaldo34', model);
+    var user = pool.getModelData('ryanaldo34') as UserModel; // cast the generic DocumentModel to UserModel
+
+    expect(user.firstname, 'Ryan');
+    expect(user.lastname, 'Monahan');
+  });
+}
