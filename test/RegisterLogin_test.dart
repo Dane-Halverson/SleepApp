c86f735ea7ed8007bc0caa959c76f809be53462c
Dane Halverson <halve564@d.umn.edu>
2023-03-19 01:15:20 -0500

    fixed merge conflicts

--- test/RegisterLogin_test.dart
+++ test/RegisterLogin_test.dart
@@ -1,9 +1,5 @@
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:intl/intl.dart';
 import '../lib/Authentication.dart';
-import 'package:firebase_auth_mocks/firebase_auth_mocks.dart';
 
 
 void main() async{
