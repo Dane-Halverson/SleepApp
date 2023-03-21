a691f3f07581b5a29503ebf9ecac2a73e09b059d
Sara Troseth <trose029@d.umn.edu>
2023-03-21 08:36:02 -0500

    Added form widgets but it does not show or navigate to proper screen

--- lib/main.dart
+++ lib/main.dart
@@ -35,7 +35,7 @@ class MyApp extends StatelessWidget {
       home: Builder(
         builder: (context) => Scaffold(
           appBar: AppBar(
-            title: Text("Sweet Dreams"),
+            title: Text("Sweet Dreams!"),
           ),
           body: Center(
             child: Column(
@@ -44,22 +44,86 @@ class MyApp extends StatelessWidget {
                   padding: EdgeInsets.only(top: 20.0, bottom: 20.0),
                   child: Text("Sweet Dreams!",style: const TextStyle(fontWeight: FontWeight.bold, color: Colors.blueAccent), textScaleFactor: 3,)
                   ,),
+
+                /**
+                    Create account button
+                    - NEEDS NAV TO CORRECT SCREEN
+                **/
                 ElevatedButton(
                   style: ElevatedButton.styleFrom(
                       backgroundColor: Colors.blueAccent
                   ),
-                  child: Text('Begin'),
+                  child: Text('Create Account'),
                   onPressed: () {
                     Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context) {
                       return SplashScreen();
                     }));
                   },
                 )
-              ],
-            )
+    ])))));}}
+
+class MyCustomForm extends StatefulWidget {
+  const MyCustomForm({super.key});
+
+  @override
+  MyCustomFormState createState() {
+    return MyCustomFormState();
+  }
+}
+
+// Define a corresponding State class.
+// This class holds data related to the form.
+class MyCustomFormState extends State<MyCustomForm> {
+  // Create a global key that uniquely identifies the Form widget
+  // and allows validation of the form.
+  //
+  // Note: This is a `GlobalKey<FormState>`,
+  // not a GlobalKey<MyCustomFormState>.
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    // Build a Form widget using the _formKey created above.
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: <Widget>[
+          TextFormField(
+            decoration: const InputDecoration(
+              labelText: "E-mail"
+      ),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter a valid email';
+              }
+              return null;
+            },
           ),
-        )
-      )
+          TextFormField(
+            decoration: const InputDecoration(
+              labelText: "Password"
+            ),
+            validator: (value){
+                if (value == null || value.isEmpty){
+                  return 'Please enter a valid password';
+                }
+                return null;
+            },
+          ),
+
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blueAccent
+            ),
+            child: Text('Log In'),
+            onPressed: () {
+              Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context) {
+                return SplashScreen();
+              }));
+            },
+          )
+        ],
+      ),
     );
   }
 }
