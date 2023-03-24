6e117f660df83f76c150286b937d100da8b33846
Dane Halverson <halve564@d.umn.edu>
2023-03-23 21:49:00 -0500

    added a presenter to the settings view, fixed some bugs

--- lib/SettingsView.dart
+++ lib/SettingsView.dart
@@ -1,5 +1,6 @@
 import 'package:settings_ui/settings_ui.dart';
 import 'package:flutter/material.dart';
+import 'package:units/presenters/SettingsPresenter.dart';
 import 'contracts/settings_contract.dart';
 import 'Authentication.dart';
 
@@ -25,15 +26,20 @@ class _SettingsStatefulWidgetState extends State<SettingsStatefulWidget>
 
   Authentication auth = new Authentication();
 
+  late SettingsPresenter presenter;
 
-
+  _SettingsStatefulWidgetState() : super() {
+    presenter = new SettingsPresenter(this);
+  }
 
   @override
   Widget build(BuildContext context) {
-    late String email = auth.getUserEmail()!;
+    print(auth.isSignedIn().then((value) {return value;}));
+    late String email = auth.getUserEmail() != null ? auth.getUserEmail()! : "";
     return  Scaffold(
       appBar: AppBar(
         title: Text("Settings"),
+        backgroundColor: Colors.deepPurple,
       )
       ,
       body: SettingsList(
@@ -49,17 +55,23 @@ class _SettingsStatefulWidgetState extends State<SettingsStatefulWidget>
               SettingsTile(
                 leading: Icon(Icons.logout),
                 title: Text('Sign out'),
-                onPressed: _onSignOut(),
+                onPressed: (_) {
+                  _onSignOut();
+                },
               ),
               SettingsTile(
                 leading: Icon(Icons.password),
                 title: Text('Reset Password'),
-                onPressed: _onResetPassword(),
+                onPressed: (_) {
+                  _onResetPassword();
+                  },
               ),
               SettingsTile(
                 leading: Icon(Icons.password),
                 title: Text('Delete Account'),
-                onPressed: _onDeleteAccount(),
+                onPressed: (_) {
+                  _onDeleteAccount();
+                  },
               ),
             ],
           ),
@@ -69,15 +81,16 @@ class _SettingsStatefulWidgetState extends State<SettingsStatefulWidget>
   }
 
   _onSignOut() {
-
+    print("test");
+    presenter.onSignOut();
   }
 
   _onResetPassword() {
-
+    presenter.onResetPassword();
   }
 
   _onDeleteAccount() {
-
+    presenter.onDeleteAccount();
   }
 
   @override
