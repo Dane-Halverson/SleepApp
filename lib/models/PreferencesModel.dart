d193acb35b456eb2b92f923f74545e41664862b0
lockn017 <lockn017@d.umn.edu>
2023-04-04 14:38:38 -0500

    Made changes to the Preference Model

--- lib/models/PreferencesModel.dart
+++ lib/models/PreferencesModel.dart
@@ -1,29 +1,37 @@
-/**
- * Preferences model stores a map in the user document of the database
- *
- */
+/// Preferences model stores a map in the user document of the database
+/// Each variable will have a value assigned to it, this value will be used
+/// to present the user with the desirable information
 
+// ignore: camel_case_types
 class Preferences_Model {
   // each setting will have a variable in the preference model
   int? graphModel;
-  int? numDaystoDisplay;
-  Map<String, dynamic> preferences;
+  int? numDaysToDisplay;
+  late Map<String, dynamic> preferences = {
+  'Graph Model' : graphModel,
+  'Number of Days to Display' : numDaysToDisplay,
+};
 
-  Preferences_Model({this.graphModel, this.numDaystoDisplay, this.preferences});
-}
+  Preferences_Model({
+    required this.graphModel,
+    required this.numDaysToDisplay,
+  });
 
-int getGraphModel() {
-  return graphModel;
-}
+  int? getGraphModel() {
+    return graphModel;
+  }
 
-void setGraphModel(int graphModel) {
-  this.graphModel = graphModel;
-}
+  void setGraphModel(int graphModel) {
+    this.graphModel = graphModel;
+    preferences['Graph Model'] = graphModel;
+  }
 
-int getNumDays() {
-  return numDaystoDisplay;
-}
+  int? getNumDays() {
+    return numDaysToDisplay;
+  }
 
-void setNumDays(int numDaystoDisplay) {
-  this.numDaystoDisplay = numDaystoDisplay;
+  void setNumDays(int numDaysToDisplay) {
+    this.numDaysToDisplay = numDaysToDisplay;
+    preferences['Number of Days to Display'] = numDaysToDisplay;
+  }
 }
